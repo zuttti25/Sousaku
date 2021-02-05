abf702b69df36a409b199251f8575b2471b3fb3f
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-06 08:54:39 +0900

    Add Column Comment

--- db/migrate/20210205233109_create_comments.rb
+++ db/migrate/20210205233109_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :post, foreign_key: true, null: false
+      t.string :content, null: false
 
       t.timestamps
     end
