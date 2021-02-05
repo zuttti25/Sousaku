7eddae97e230e5cf8c937853426baeabfdbfd6b9
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-05 23:03:47 +0900

    Add UserColumn

--- db/migrate/20210205134124_devise_create_users.rb
+++ db/migrate/20210205134124_devise_create_users.rb
@@ -32,7 +32,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name, null: false, default: ""
+      t.string :image_id
+      t.text :introduction
+      t.string :field
+      t.string :link
+      t.boolean :is_deleted, null: false, default: false
       t.timestamps null: false
     end
 
