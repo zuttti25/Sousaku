98ec0ac019eea77713cefacdfabe68953bff9db0
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-06 09:13:59 +0900

    Add Column Post

--- db/migrate/20210205233127_create_posts.rb
+++ db/migrate/20210205233127_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.references :user, foreign_key: true, null: false
+      t.integer :category, null: false
+      t.string :title, null: false
+      t.text :content, null: false
+      t.string :image_id
+       
       t.timestamps
     end
   end
