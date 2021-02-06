ae1a2eb97efe8faf14a3ad2c90e86b8483b14da7
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-07 08:50:22 +0900

    Add device/link

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  + before_action :check_guest, only: %i[update destroy]
+  before_action :check_guest, only: %i[update destroy]
 
 end
