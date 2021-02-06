f2361e274261be9211540b64d3cae39bd451a768
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-06 16:36:40 +0900

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#top'
+  resources :users, only: [:show, :edit, :update] 
+  #do
+  # resource :relationships, only: [:create, :destroy]
+    # get :follows, on: :member
+    # get :followers, on: :member
+  # end
+  #resources :rooms, only: [:show, :index]
+  #resources :messages, only: [:create]
+  #resources :notifications, only: [:index, :destroy]
 end
