8062fb4d6fe8afabf2b28b2e820b09998f7fe07e
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-06 09:16:14 +0900

    Fix Column Post

--- db/migrate/20210205233127_create_posts.rb
+++ db/migrate/20210205233127_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.references :user, foreign_key: true, null: false
-      t.integer :category, null: false
+      t.integer :category_id, null: false
       t.string :title, null: false
       t.text :content, null: false
       t.string :image_id
