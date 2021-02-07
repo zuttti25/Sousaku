b804b52407b357b3f455eb4bed28fbf02fd6cdd1
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-07 10:50:55 +0900

    Add user edit/inputspace

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
   def show
   end
-
+  
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :field, :link)
+  end
+
 end
