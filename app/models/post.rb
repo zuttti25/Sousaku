078e1a0e4ce79e9144ecca2e8b945d59baf1c044
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-07 15:44:49 +0900

    Add user/image/form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+
+attachment :image
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
+  attachment :image
+  
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
-  
+
 end
