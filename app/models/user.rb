f0872f6f46ac3e5763f17f0d322e33b6d2eb1d4e
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-09 08:38:14 +0900

    Add DM associate

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
+  # DM機能のアソシエーション
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
