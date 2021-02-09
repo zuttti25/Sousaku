0cbdb44a40370c7bb5db32f351f451a98861dcd4
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-09 16:32:34 +0900

    FIX header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+ 
+  def edit
+    @user=User.find(params[:id])
   end
-  
+
   def show
     @user=User.find(params[:id])
     @currentUserEntry=Entry.where(user_id: current_user.id)
