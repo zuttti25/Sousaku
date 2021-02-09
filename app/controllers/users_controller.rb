e0f6581b92171af15d50c9eb8e094c133bde1120
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-09 12:08:12 +0900

    Add images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,18 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
     @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
+    @userEntry=Entry.where(user_id: @user.id)  #@userが現在ログインしているユーザーではない場合
+    #すでにroomsが"作成されている場合"と"作成されていない場合"に条件分岐
+    unless @user.id == current_user.id
       @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
+        @userEntry.each do |u|   
+          if cu.room_id == u.room_id then  #ユーザー同士のルームIDを照合する
             @isRoom = true
             @roomId = cu.room_id
           end
         end
       end
-      if @isRoom
-      else
+      unless @isRoom  #ログイン中のユーザーのroom_idがfalse（存在しない）の場合、新しくルームを作成する
         @room = Room.new
         @entry = Entry.new
       end
