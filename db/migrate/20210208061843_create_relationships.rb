433cbd0b5d2cc697cd8fabc013de0ca0328c70d0
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-18 10:27:17 +0000

    Fix relationships.rb

--- db/migrate/20210208061843_create_relationships.rb
+++ db/migrate/20210208061843_create_relationships.rb
@@ -1,4 +1,4 @@
-class CreateRelationships < ActiveRecord::Migration[5.0]
+class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
       t.references :user, foreign_key: true
