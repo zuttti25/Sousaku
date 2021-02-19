749801527d44be6a2ba02cb1aec87f8a18874651
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-19 04:19:04 +0000

    Finish modal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = true
-config.assets.compile = true
+  config.assets.compile = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
