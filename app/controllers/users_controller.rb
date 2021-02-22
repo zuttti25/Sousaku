e6dac15e84743087bbc01e9a18926ba3573ea94a
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-22 12:17:09 +0000

    Fix usersshow//update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(current_user)
+ 
     else
       render :edit
     end
