4dca41aeaee32461a5ab707bc29b68aae3339ed1
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-24 16:45:57 +0000

    Fix board/show

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,9 @@
 class HomesController < ApplicationController
 
   def top
-      @users = User.all
-      @pickup = Post.limit(8).order("created_at DESC")
-      @popular = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(4).pluck(:post_id))
+      @users = User.all.limit(12)
+      @pickup = Post.limit(12).order("created_at DESC")
+      @popular = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(12).pluck(:post_id))
       @boards = Board.page(params[:page]).per(4).order("created_at DESC")
     #@boards = Board.all
   end
