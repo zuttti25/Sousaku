0b34c9cda6fceed8e4ada4f6da44f3644ba97a48
zuttti25 <xxx.pxjpp@icloud.com>
2021-02-28 14:49:47 +0000

    Add root/top

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
 
-  
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions:'users/sessions'
   }
-  
-  
-  
+
+
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
-  
+
   get "/posts/pickup", to: 'posts#pickup', as: 'pickup'
   get "/posts/popular", to: 'posts#popular', as: 'popular'
   resources :posts do
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   resources :boards
   get "/users/:id/mypost", to: 'users#mypost', as: 'mypost'
   get "/users/:id/mylike", to: 'users#mylike', as: 'mylike'
-  resources :users, only: [:index, :show, :edit, :update] 
+  resources :users, only: [:index, :show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
   get "/users/:id/relationships/followers", to: 'relationships#followings', as: 'followings'
   get "/users/:id/relationships/followings", to: 'relationships#followers', as: 'followers'
@@ -32,5 +32,5 @@ Rails.application.routes.draw do
   get "search" => "users#search"
   #get "top" => "homes#top"
   #root 'homes#about'
-  root 'homes#'
+  root 'homes#top'
 end
