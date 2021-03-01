ea624d75cf9d5e46b85f670d22cdb9abe1116df1
zuttti25 <xxx.pxjpp@icloud.com>
2021-03-01 09:03:07 +0000

    Fix top/button user/button

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   def top
       @users = User.limit(12).order("created_at DESC")
       @pickup = Post.limit(12).order("created_at DESC")
-      @popular = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
+      @popular = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
       #@boards = Board.page(params[:page]).per(4).order("created_at DESC")
       @boards = Board.all.order("created_at DESC")
   end
