d1ca34c621ab7f05b159a359d8e96ff0c669d618
zuttti25 <xxx.pxjpp@icloud.com>
2021-03-11 07:52:07 +0000

    Finish message

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,15 @@
 class RoomsController < ApplicationController
   
   def index
-    @rooms = Room.all
+    @user = User.find_by(id: current_user.id)
+    @currentEntries = current_user.entries
+    myRoomIds = []
+
+    @currentEntries.each do |entry|
+      myRoomIds << entry.room.id
+    end
+
+    @anotherEntries = Entry.where(room_id: myRoomIds).where('user_id != ?',@user.id)
   end
-  
+
 end
