350295253d63e753b28b88f335f956c99870b4b8
zuttti25 <xxx.pxjpp@icloud.com>
2021-03-11 02:46:38 +0000

    Add product.config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,10 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'ws://Elastic IP/cable'
+  config.action_cable.allowed_request_origins = [ 'http://Elastic IP' ]  
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
