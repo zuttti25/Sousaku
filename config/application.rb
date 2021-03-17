3fee2a3e2e00a6d12cf5d652480c003c4d11960f
zuttti25 <xxx.pxjpp@icloud.com>
2021-03-17 13:25:20 +0000

    Save devise

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Sousaku
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
